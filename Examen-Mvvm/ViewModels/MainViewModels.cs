0cb1bcb7b8c05f47f99303778751a6f630fbdd9c
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-03-26 18:52:27 -0600

    Binding

--- Examen-Mvvm/ViewModels/MainViewModels.cs
+++ Examen-Mvvm/ViewModels/MainViewModels.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Examen_Mvvm.ViewModels;
 
@@ -12,5 +13,55 @@ public partial class MainViewModels:ObservableObject
     
     [ObservableProperty]
     private double producto3;
+    
+    [ObservableProperty]
+    private double descuento;
+    
+    [ObservableProperty]
+    private double total;
+    
+    [ObservableProperty]
+    private double suma;
+
+    [RelayCommand]
+    private void Calcular()
+    {
+       Suma= producto1 + producto2 + producto3;
+
+       if (Suma>10000)
+       {
+           Descuento = Suma * 0.3;
+       }
+       else if (Suma>5000)
+       {
+           Descuento = Suma * 0.2;
+       }
+       else if (Suma>1000)
+       {
+           Descuento = Suma * 0.1;
+       }
+       else
+       {
+           Descuento = 0;
+       }
+
+       Total = Suma - Descuento;
+    }
+    
+    [RelayCommand]
+    private void Limpiar()
+    {
+        Producto1 = 0;
+        Producto2 = 0;
+        Producto3 = 0;
+        Suma = 0;
+        Descuento = 0;
+        Total = 0;
+    }
+    
+    private void Alerta(string Titulo, string Mensaje)
+    {
+        MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(Titulo, Mensaje, "Aceptar"));
+    }
     
 }
--- Examen-Mvvm/Views/MainView.xaml.cs
+++ Examen-Mvvm/Views/MainView.xaml.cs
@@ -1,9 +1,16 @@
+using Examen_Mvvm.ViewModels;
+
 namespace Examen_Mvvm.Views;
 
 public partial class MainView : ContentPage
 {
+	
+	private MainViewModels viewModel;
 	public MainView()
 	{
 		InitializeComponent();
+		viewModel = new MainViewModels();
+		BindingContext = viewModel;
+
 	}
 }
