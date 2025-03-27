68aaf3092343f5239f6b4d8cef22b61f2e3d21f3
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-03-26 18:36:16 -0600

    ObservableObject

---
+++ Examen-Mvvm/ViewModels/MainViewModels.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Examen_Mvvm.ViewModels;
+
+public partial class MainViewModels:ObservableObject
+{
+    [ObservableProperty] 
+    private double producto1;
+    
+    [ObservableProperty]
+    private double producto2;
+    
+    [ObservableProperty]
+    private double producto3;
+    
+}
